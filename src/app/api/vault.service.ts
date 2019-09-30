19558346a8e4fddc9905526f1d71a62aa113a480
Joe <joe.bell.1329@gmail.com>
2019-09-30 16:40:32 +0100

    implement vault creation and editing

--- src/app/api/vault.service.ts
+++ src/app/api/vault.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IEncryptedData } from '../crypto/crypto.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,15 @@ export class VaultService {
   constructor(public httpClient: HttpClient) {}
 
   public getVault(email: string) {
-    return this.httpClient.get(`http://localhost:8000/vault`, { params: { email } });
+    return this.httpClient.get('http://localhost:8000/vault', { params: { email } });
+  }
+
+  public createVault(email: string, payload: IEncryptedData) {
+    return this.httpClient.post('http://localhost:8000/vault', { email, payload });
+  }
+
+  public updateVault(email: string, payload: IEncryptedData) {
+    return this.httpClient.put('http://localhost:8000/vault', { email, payload });
   }
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { combineLatest, interval, Observable, of, Subject } from 'rxjs';
-import { catchError, map, shareReplay, switchMap } from 'rxjs/operators';
+import { catchError, map, shareReplay, switchMap, take, tap } from 'rxjs/operators';
 
 import { IVault, IVaultTokens } from './vault/vault.model';
 import { IEncryptedData } from './crypto/crypto.model';
@@ -20,11 +20,16 @@ export class AppComponent implements OnInit {
   public timeToNext$: Observable<number>;
   public vaultAuth$ = new Subject<{ email: string, password: string }>();
 
+  public createEmail: string;
+  public createPassword: string;
+
   public password: string;
   public email: string;
 
-  constructor(private vaultManagementService: VaultManagementService, private vaultService: VaultService) {
-  }
+  public newTokenName: string;
+  public newTokenSecret: string;
+
+  constructor(private vaultManagementService: VaultManagementService, private vaultService: VaultService) {}
 
   ngOnInit(): void {
     this.lockedVault$ = this.vaultAuth$
@@ -40,7 +45,9 @@ export class AppComponent implements OnInit {
         catchError((err: Error) => {
           alert(err.message);
           return of([]);
-        })
+        }),
+        tap(console.log),
+        shareReplay(1)
       );
 
     this.tokens$ = interval(1000)
@@ -55,5 +62,33 @@ export class AppComponent implements OnInit {
         const marker = seconds >= 30 ? 60 : 30;
         return marker - seconds;
       }));
+  }
+
+  createVault() {
+    const newVault: IVault = [];
+    this.vaultManagementService.lockVault(newVault, this.createPassword)
+      .pipe(
+        switchMap(newLockedVault => this.vaultService.createVault(this.createEmail, newLockedVault))
+      )
+      .subscribe(response => {
+        console.log(response);
+        this.vaultAuth$.next({ email: this.createEmail, password: this.createPassword });
+      });
+  }
+
+  addToken() {
+    this.unlockedVault$
+      .pipe(
+        take(1),
+        map(unlockedVault => {
+          return [ ...unlockedVault, {name: this.newTokenName, secret: this.newTokenSecret}];
+        }),
+        switchMap(unlockedVault => this.vaultManagementService.lockVault(unlockedVault, this.password)),
+        switchMap(lockedVault => this.vaultService.updateVault(this.email, lockedVault))
+      )
+      .subscribe(response => {
+        console.log(response);
+        this.vaultAuth$.next({ email: this.email, password: this.password });
+      });
   }
 }
